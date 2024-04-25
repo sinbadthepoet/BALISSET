19dd20fe00f5d6febb779b5ca2b606b281772f4e
HaytharAli <Haiderali360@gmail.com>
2024-04-25 12:35:50 -0400

    Input Actions Defined

---
+++ BALISSET/Assets/B_Scripts/Objects/B_Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class B_Player : MonoBehaviour
+{
+    [SerializeField] Camera _camera;
+
+    #region Action Functions
+    void Movement()
+    {
+
+    }
+
+    void Look()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+
+    void Crouch()
+    {
+
+    }
+
+    void Sprint()
+    {
+
+    }
+
+    void Interact()
+    {
+
+    }
+    #endregion
+}
